b123a8a562b5eedb7c15a55120d21d907c2775cc
CAIODIASDASILVA <127319215+CAIODIASDASILVA@users.noreply.github.com>
2023-11-22 11:31:00 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-alert("test")
+document.querySelector('#som-tecla-clap').play()
+
+function tocaSomPom(){
+    document.querySelector('#som-tecla-pom').play()
+}
+alert("teste")
+const listarDeTEclas = documente.querySelectorAll('.tecla')
+
+let contador = 0;
+
+while (contador < listarDeTeclas.Length){
+    listarDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador + 1;
+}

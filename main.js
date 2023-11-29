2cfda08b3310190bd693cda13ef5cc568a620e79
CAIODIASDASILVA <127319215+CAIODIASDASILVA@users.noreply.github.com>
2023-11-29 11:18:14 -0300

    .

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const listarDeTeclas = document.querySelectorAll('.tecla')
 
 let contador = 0;
 
-while (contador < listarDeTeclas.lenght ){
+while (contador < listarDeTeclas.length){
     const instrumento = listarDeTeclas[contador].classList
         console.log (instrumento[1])
         listarDeTeclas[contador].onclick = function (){

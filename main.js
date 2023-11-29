eaff986741bb3f2c67fd2996ff0a32a600ccd3cf
CAIODIASDASILVA <127319215+CAIODIASDASILVA@users.noreply.github.com>
2023-11-29 11:09:27 -0300

    .

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-function tocaSomPom(idElementoAudio){
+function tocaSompom(idElementoAudio){
     document.querySelector(idElementoAudio).play()
 }
 
@@ -9,7 +9,7 @@ const listarDeTeclas = document.querySelectorAll('.tecla')
 let contador = 0;
 
 while (contador < listarDeTeclas. lenght ){
-    .const instrumento = listarDeTeclas[contador].classList
+    const instrumento = listarDeTeclas[contador].classList
         console.log (instrumento[1])
         listarDeTeclas[contador].onclick = function (){
 
